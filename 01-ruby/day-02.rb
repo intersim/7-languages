05635c6055de93da324919d8bc1141c1bb783314
Emily Intersimone <emily.intersimone@gmail.com>
2017-04-21 15:31:59 -0400

    add tree class

--- 01-ruby/day-02.rb
+++ 01-ruby/day-02.rb
@@ -32,12 +32,12 @@ h.values   #=> [100, 200, 300]
 
 # what other common data structures do arrays support?
 
-# Print the contents of an array of sixteen numbers four numbers at a time, using just each. Now, do the same with each_slice in Enumerate.
-arr = []
+# # Print the contents of an array of sixteen numbers four numbers at a time, using just each. Now, do the same with each_slice in Enumerate.
+# arr = []
 
-for i in 1..16
-  arr.push(i)
-end
+# for i in 1..16
+#   arr.push(i)
+# end
 
 # arr.each do |el|
 #   idx = arr.index(el)
@@ -46,9 +46,46 @@ end
 #   end
 # end
 
-arr.each_slice(4).to_a.each {|a| puts a}
+# arr.each_slice(4).to_a.each {|a| print "#{a[0]} #{a[1]} #{a[2]} #{a[3]}\n"}
+
+# arr.each_slice(4).to_a.each do |a|
+#   (0..3).each {|i| print "#{a[i]}"}
+#   puts "\n"
+# end
 
 # The Tree class was interesting, but it did not allow you to specify a new tree with a clean user interface. Let the initializer accept a nested structure with hashes and array. You should be able to specify a tree like this:
 # `{'grandpa' => { 'dad' => { 'child 1' => {}, 'child 2' => {} }, 'uncle' => { 'child 3' => {}, 'child 4' => {} } }}
+
+class Tree
+  attr_accessor :children, :node_name
+
+  def initialize(name, children=[])
+    @children = children
+    @node_name = name
+  end
+
+  def visit_all(&block)
+    visit &block
+    children.each {|c| c.visit_all &block}
+  end
+
+  def visit(&block)
+    block.call self
+  end
+end
+
+=begin
+ruby_tree = Tree.new('Ruby',
+  [
+    Tree.new('Reia'),
+    Tree.new('MacRuby')
+  ])
+
+puts 'Visiting a node'
+ruby_tree.visit {|node| puts node.node_name}
+
+puts 'visiting entire tree'
+ruby_tree.visit_all {|node| puts node.node_name}
+=end
 
 # Write a simple grep that will print the lines of a file having any occurrences of a phrase anywhere in that line. You will need to do a simple regular expression match and read lines from a file. (This is surprisingly simple in Ruby.) If you want, include line numbers.
