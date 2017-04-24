c36bd6c724909ad6fdbc901ce4a117b473936fc2
Emily Intersimone <emily.intersimone@gmail.com>
2017-04-24 14:22:33 -0400

    get dummy CSV data

--- 01-ruby/day-03.rb
+++ 01-ruby/day-03.rb
@@ -8,10 +8,54 @@ lions, tigers
 
 allow an API that works like this:
 
-csv = Ruby.Csv.new
+csv = RubyCsv.new
 csv.each {|row| puts row.one}
 
 This should print "lions".
 
 =end
+
+module ActsAsCsv
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+
+    def each
+
+    end
+
+    attr_accessor :headers, :csv_contents
+    def initialize
+      read
+    end
+  end
+end
+
+class RubyCsv  # no inheritance! You can mix it in
+  include ActsAsCsv
+  acts_as_csv
+end
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_contents.inspect
+
 
